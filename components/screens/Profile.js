1abaa64c489202647fcfd15418de9ba9a1496c98
Faris Widyantho <radenfaris@gmail.com>
2018-01-12 10:14:24 +0700

    add tab navigate to profile

--- components/screens/Profile.js
+++ components/screens/Profile.js
@@ -2,9 +2,35 @@ import React from 'react'
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Dimensions
 } from 'react-native'
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 import { connect } from 'react-redux'
+
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get('window').width,
+};
+
+const youTodayRoute = () => {
+  return (
+    <View style={ styles.tabContainer }>
+      <Text style = {{ fontSize: 20 }}>Status: 🚶 walk/run</Text>
+      <Text style = {{ fontSize: 20 }}>Steps Count: 42/6000*</Text>
+      <Text style = {{ fontSize: 20 }}>Drink Count: 0.2/2.1 liters</Text>
+    </View>
+  )
+};
+
+const historyRoute = () => {
+  return (
+    <View style={ styles.tabContainer }>
+      <Text style = {{ fontSize: 20 }}>Steps per day: 666</Text>
+      <Text style = {{ fontSize: 20 }}>Drink per day: 2.0 liters</Text>
+    </View>
+  )
+};
 
 class Profile extends React.Component {
   static navigationOptions = {
@@ -13,15 +39,35 @@ class Profile extends React.Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      index: 0,
+      routes: [
+        { key: 'youToday', title: 'You Today' },
+        { key: 'history', title: 'History' },
+      ],
+    }
   }
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    youToday: youTodayRoute,
+    history: historyRoute,
+  });
 
   render() {
     return (
       <View style = { styles.container }>
-        <View style = {{ marginLeft: 20 }}>
-          <Text>This is Profile</Text>
-        </View>
+        <TabViewAnimated
+          style={styles.container}
+          navigationState={this.state}
+          renderScene={this._renderScene}
+          renderHeader={this._renderHeader}
+          onIndexChange={this._handleIndexChange}
+          initialLayout={initialLayout}
+        />
       </View>
     )
   }
@@ -31,7 +77,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
-    paddingTop: 30
+  },
+  tabContainer: {
+    marginRight: 20,
+    marginLeft: 20,
+    marginTop: 20,
+    marginBottom: 20,
   }
 })
 
