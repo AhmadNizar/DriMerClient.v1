01cd5bb90b6528e648a633a52e08ecee8066e23d
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-14 15:48:16 +0700

    Revert "fixing some bugs" unmerge tab-bottle
    This reverts commit 72cb8c3ab3f16866baa22980308e538f5ac7a503.

--- components/screens/Register.js
+++ components/screens/Register.js
@@ -31,7 +31,8 @@ class Register extends React.Component {
     this.handleInputAge = this.handleInputAge.bind(this)
   }
   register = () => {
-    if (this.state.email === '' || this.state.age === '' || this.state.gender === '' || this.state.nameValue === '' || this.state.password === '') {
+    console.log("Masuk register....")
+    if (this.state.email === '' || this.state.age === '' || this.state.gender === '' || this.state.nameValue === '') {
       this.setState({
         errorMessage: 'All forms must be filled.'
       })
@@ -135,7 +136,7 @@ class Register extends React.Component {
           <View style={styles.input}>
             <TextInput underlineColorAndroid='rgba(0,0,0,0)' secureTextEntry placeholder="Password" onChangeText={(text) => this.setState({ password: text })} />
           </View>
-          <SocialIcon button style={{ backgroundColor: '#296666' }} title='Register' onPress={this.register} />
+          <Button color='#296666' title='Register' onPress={this.register} />
         </View>
       </View>
     );
